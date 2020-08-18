50d42bfebf0dd96313554de00e550b58169d64cb
Hao Dam <hao.dam@gmail.com>
2020-08-19 00:00:46 +0100

    Fixed multiplication3

--- test_calculator.py
+++ test_calculator.py
@@ -12,6 +12,6 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
-    
+
     def test_multiplication(self):
         assert 12 == calculator.multiply(3, 4)
